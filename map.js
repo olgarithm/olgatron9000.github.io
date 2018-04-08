6ed072474887349bb8f59bf383ea086a109f9fd3
Olga Andreeva <andreolg@cs.washington.edu>
2018-04-08 13:08:34 -0700

    Add resume and favicon

---
+++ map.js
@@ -0,0 +1,30 @@
+
+(function () {
+   "use strict";
+   
+   var options = {
+    width: "100%",
+    height: "100%",
+    pdfOpenParams: {
+        toolbar: 0
+      }
+   };
+
+	// Given an id, returns the corresponding element
+	function $(id) { return document.getElementById(id); }
+
+	// Given a selector, returns all the elements that match that selection
+	function qsa(sel) { return document.querySelectorAll(sel); }
+
+	// Give a selector, returns the first element that matches that selection
+	function qs(sel) { return document.querySelector(sel); }
+   
+	window.onload = function() {
+	$("resumeclick").onclick = getResume;
+   };
+   
+   function getResume() {
+      $("resume").classList.toggle("hidden");
+      PDFObject.embed("Olga-Andreeva-Resume.pdf", "#resume", options);
+   }
+})();
