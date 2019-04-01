9462ce1aee7d20f82a8af5a6d490d2755e50dcb1
Olga Andreeva <andreolg@cs.washington.edu>
2019-04-01 15:35:07 -0700

    Add creation section

---
+++ js/infographic-load.js
@@ -0,0 +1,24 @@
+(function () {
+   "use strict";
+   
+   var options = {
+    width: "1395px",
+    height: "1125px",
+    pdfOpenParams: {
+        toolbar: 0
+      }
+   };
+
+	// Given an id, returns the corresponding element
+	function $(id) { return document.getElementById(id); }
+
+	// Given a selector, returns all the elements that match that selection
+	function qsa(sel) { return document.querySelectorAll(sel); }
+
+	// Give a selector, returns the first element that matches that selection
+	function qs(sel) { return document.querySelector(sel); }
+   
+	window.onload = function() {
+	 PDFObject.embed("../pdf/Rotten-Tomatoes-Infographic.pdf", "#infographic", options);
+  	};
+})();
